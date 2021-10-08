83aab75cecb23a9b9188a42a06a3b1470bb7b986
Emilio Pardo Pérez <e.pardo.perez@gmail.com>
2021-10-08 14:15:04 +0200

    Update index.js

--- visores/visor1/javascript/index.js
+++ visores/visor1/javascript/index.js
@@ -15,47 +15,16 @@ const map = M.map({
 map.addControls(['scaleline', 'mouse', 'OverviewMap', 'panzoombar', 'layerswitcher']);
 
 
-// configuración plugin simpleLegend
+mapa.addControls(new M.control.GetFeatureInfo(
+ 'html', 
+ {buffer: 10}));
 
-const configSimpleLegend = {
-    title: 'Leyenda',
-    draggable: true,
-    layers: []
-}
 
 // configuración plugin simpleBaseLayerSelector
 
 const configSimpleBaseLayerSelector = { displayBaseLayersInLayerSwitcher: false }
 
 // configuración plugin inputSelectAddLayer
-const configTipologiasConstrucctivas = {
-    title: 'Tipologías constructivas de catastro en malla estadística',
-    label: 'Tipologías constructivas de catastro en malla estadística',
-    group: false,
-    data: {
-      name: 'Tipologias Construcctivas',
-      layers: [
-        viviendas_colectivas,
-        viviendas_unifamiliares,
-        edificiacion_rural,
-        garages_viviendas,
-        otros_garajes,
-        industrial,
-        oficinas,
-        comercio,
-        deporte,
-        piscinas,
-        espectaculos,
-        hosteleria_ocio,
-        sanitarios_beneficos,
-        culturales,
-        religiosos,
-        singulares,
-        campo_golf
-      ]
-    }
-  }
-
 
   const configDirectorioEmpresas = {
     title: 'Geolocalización de Establecimientos de 50 o más Asalariados. Directorio de Empresas y Establecimientos',
@@ -76,57 +45,6 @@ const configTipologiasConstrucctivas = {
   }
 
 
-const configInventarioSedesEquipamientos = {
-    title: 'Inventario de Sedes y Eqipamientos',
-    label: 'ISE',
-    group: true,
-    data: [
-        {
-            name: 'Sedes Administrativas',
-            layerGroups: [
-                {
-                    group: 'Sedes',
-                    layers: [
-                        registros,
-                        buzones,
-                        delegaciones,
-                        archivos,
-                        organizacionInstitucional,
-                        consejerias,
-                        organismos
-                    ]
-                }
-            ]
-        },
-        {
-            name: 'Equipamientos',
-            layerGroups: [
-                {
-                    group: 'Centros y equipamientos',
-                    layers: [
-                        agricultura,
-                        cultura,
-                        empresa,
-                        educacion,
-                        empleo,
-                        industria,
-                        investigacion,
-                        justicia,
-                        medioAmbiente,
-                        otros,
-                        salud,
-                        social,
-                        tecnologia,
-                        transporte,
-                        turismo
-                    ]
-                }
-            ]
-        }
-    ]
-}
-
-
 const configIECA = {
   open: true,
   htmlCode: '<div id="social"  title="Compartir"> <ul> <li class="no_imprimir"><a id="btnCompartir" href="#"><img src="https://www.juntadeandalucia.es/institutodeestadisticaycartografia/portal/images/icons/social/compartir24g.png" border="0" alt="Compartir">&nbsp;<span>Compartir</span></a> <ul id="soc_compartir"> <li><a href="https://www.facebook.com/sharer.php?u='+window.location.href+'&amp;t=+Visualizador+IECA&amp;d=" title="Compartir en Facebook" target="_blank"><img src="https://www.juntadeandalucia.es/institutodeestadisticaycartografia/portal/images/icons/social/ico_facebook.png" border="0" alt="Compartir en Facebook">&nbsp;<span>Facebook</span></a></li><li><a href="https://twitter.com/home?status=Visualizador-'+ window.location.href+'%20v%C3%ADa%20@IECA_Andalucia" title="Compartir en Twitter" target="_blank"><img src="https://www.juntadeandalucia.es/institutodeestadisticaycartografia/portal/images/icons/social/ico_twitter.png" border="0" alt="Compartir en Twitter">&nbsp;<span>Twitter</span></a></li><li><a href="https://www.linkedin.com/shareArticle?mini=true&amp;url='+ window.location.href+'&amp;title=+Visualizador+IECA" title="Compartir en LinkedIn" target="_blank"><img src="https://www.juntadeandalucia.es/institutodeestadisticaycartografia/portal/images/icons/social/ico_linkedin.png" border="0" alt="Compartir en LinkedIn">&nbsp;<span>LinkedIn</span></a></li><li><a href="mailto:?subject=Compartiendo%20por%20correo%20electr%C3%B3nico&amp;body=%20Visualizador:%0D%0A'+ window.location.href+'" title="Compartir por correo electr&oacute;nico" target="_blank"><img src="https://www.juntadeandalucia.es/institutodeestadisticaycartografia/portal/images/icons/social/ico_mail.png" border="0" alt="Compartir por correo electr&oacute;nico">&nbsp;<span>e-mail</span></a></li></ul> </li></ul> </div><img src="https://www.juntadeandalucia.es/institutodeestadisticaycartografia/portal/images/icons/Logo_UE_FEDER.gif" class="feder" alt="Fondos"><div id="footer"> <div id="piea"> <div class="lineacolor fondocolor1">&nbsp;</div><div>&nbsp;</div><div> <div id="piea1"> <a href="https://www.juntadeandalucia.es/institutodeestadisticaycartografia/ieagen/mapaWeb/index.htm" class="enlaces" accesskey="m">Mapa web</a> | <a href="https://www.juntadeandalucia.es/institutodeestadisticaycartografia/ieagen/avisoLegal/index.htm" class="enlaces" accesskey="l">Aviso legal</a> | <a href="https://www.juntadeandalucia.es/institutodeestadisticaycartografia/protecciondedatos/index.html" class="enlaces" accesskey="p">Protecci&oacute;n de datos</a> | <a href="https://www.juntadeandalucia.es/institutodeestadisticaycartografia/buzon_web/index.jsp" class="enlaces" accesskey="o">Tu opini&oacute;n</a> | <a href="https://www.juntadeandalucia.es/institutodeestadisticaycartografia/ieagen/accesibilidad/index.htm" class="enlaces" accesskey="a">Accesibilidad</a> <img src="https://www.juntadeandalucia.es/institutodeestadisticaycartografia/portal/images/images/w3caa.jpg" alt="Logo de w3c" id="logow3caa"> <a href="https://www.juntadeandalucia.es/institutodeestadisticaycartografia/ieagen/avisoLegal/index.htm#cc" class="enlaceimg"><img src="https://www.juntadeandalucia.es/institutodeestadisticaycartografia/portal/images/images/cc-by.png" title="Publicado bajo Licencia Creative Commons - Reconocimiento 4.0" alt="CC-BY 4.0" id="logoccby"></a> </div><div id="piea2"> Pabell&oacute;n de Nueva Zelanda. <br>C/ Leonardo Da Vinci, n&deg; 21. Isla de La Cartuja. 41071-SEVILLA.<br>Tlf.: <strong>900 101 407/955 033 800</strong> | Fax: <strong>955 033 816</strong><br></div></div></div></div>',
@@ -139,8 +57,6 @@ const configIECA = {
 
    
 const baseLayerSelector = new M.plugin.Simplebaselayerselector(configSimpleBaseLayerSelector);
-//const inputSelectAddLayer = new M.plugin.Inputselectaddlayer(configInventarioSedesEquipamientos);
-//const inputSelectAddLayer = new M.plugin.Inputselectaddlayer(configTipologiasConstrucctivas);
 const inputSelectAddLayer = new M.plugin.Inputselectaddlayer(configDirectorioEmpresas);
 const simpleLegend = new M.plugin.Simplelegend()
 const mapfooter = new M.plugin.Mapfooter(configIECA);
