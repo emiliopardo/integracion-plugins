347d6a2b23de0544d711f1bf4e26e8aa7601eb6d
Emilio Pardo <e.pardo.perez@gmail.com>
2022-04-08 08:16:51 +0200

    se crea visor de ejemplo mvtstyles

---
+++ visores/visor4/javascript/index.js
@@ -0,0 +1,34 @@
+
+let base = new M.layer.WMS({
+  url: 'http://www.ideandalucia.es/wms/mta400v_2008?',
+  name: 'Redes_energeticas',
+  legend: 'Redes',
+  transparent: false,
+  tiled: false
+})
+
+base.displayInLayerSwitcher = false;
+base.setOpacity(0);
+
+
+const map = M.map({
+  container: 'map',
+  projection: 'EPSG:3857*m',
+  layers: [base],
+  controls: ['panzoom', 'layerswitcher', 'mouse'],
+});
+
+
+// const config = 'http://localhost/tileserver/styles/mapa_andalucia_sepia_scale/style.json'
+// const config = 'http://localhost/tileserver/styles/mapa_andalucia/style.json'
+// const config = 'http://localhost/tileserver/styles/osm-liberty/style.json'
+// const config = 'http://localhost/tileserver/styles/style-basic/style.json'
+// const config = 'http://localhost/tileserver/styles/mapa_andalucia_gray_scale_fondo_negro/style.json'
+// const config = 'http://localhost/tileserver/styles/style-dark-matter/style.json'
+const config = 'https://api.maptiler.com/maps/openstreetmap/style.json?key=NxfrAN8VvDV2T3sw3L6z'
+
+const mvtStyles = new  new M.plugin.Mvtstyles(config);
+
+map.addPlugin(mvtStyles);
+
+
