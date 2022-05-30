5a70e88286c5738864ba6e95b595aee29cc36ab2
emiliopardo <e.pardo.perez@gmail.com>
2022-05-30 12:06:19 +0200

    se crea index.js para visor8

---
+++ visores/visor8/javascript/index.js
@@ -0,0 +1,41 @@
+
+
+const mapjs = M.map({
+  container: 'map',
+  projection: 'EPSG:3857*m',
+  controls: ['layerswitcher'],
+  layers: [new M.layer.OSM(),new M.layer.WMS({
+    url: 'https://ideandalucia.es/wms/ortofoto2016?',
+    name: 'ortofotografia_2016_rgb',
+    legend: 'Ortofoto 2016',
+    version: '1.1.1',
+    transparent: false,
+    tiled: true
+  })],
+});
+// Capa MVT 1
+let mapa_andalucia_color = new M.layer.MapboxStyle({
+  url: 'https://ws205.juntadeandalucia.es/tileserver/data/mapa_andalucia/{z}/{x}/{y}.pbf',
+  style: 'https://ws205.juntadeandalucia.es/tileserver/styles/mapa_andalucia/style.json',
+  name: 'Mapa de Andalucia',
+  sourceName: 'mapa_andalucia',
+  projection: 'EPSG:3857',
+});
+// Capa MVT 2
+let mapa_andalucia_fondo_negro = new M.layer.MapboxStyle({
+  url: 'https://ws205.juntadeandalucia.es/tileserver/data/mapa_andalucia/{z}/{x}/{y}.pbf',
+  style: 'https://factorylab02.ieca.junta-andalucia.es/tileserver/styles/mapa_andalucia_gray_scale_fondo_negro/style.json',
+  name: 'Mapa de Andalucia fondo negro',
+  sourceName: 'mapa_andalucia',
+  projection: 'EPSG:3857',
+});
+// Capa MVT 3
+let osm_andalucia_color = new M.layer.MapboxStyle({
+  url: 'https://factorylab02.ieca.junta-andalucia.es/tileserver/data/mapa_andalucia_osm/{z}/{x}/{y}.pbf',
+  style: 'https://factorylab02.ieca.junta-andalucia.es/tileserver/styles/style-osm-bright/style.json',
+  name: 'Open Street Maps',
+  sourceName: 'mapa_andalucia_osm',
+  projection: 'EPSG:3857',
+});
+
+mapjs.addLayers([mapa_andalucia_color, osm_andalucia_color, mapa_andalucia_fondo_negro]);     
